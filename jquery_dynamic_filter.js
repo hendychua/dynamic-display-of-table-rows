a2d436667539878847f7ea363dd770eeb2da7ced
hendychua <hendychua@gmail.com>
2012-07-14 17:28:54 -0700

    added option for user to choose within fading or normal hide/show style. catered to cases where user doesn't specify columns to search on - will search on all columns

--- jquery_dynamic_filter.js
+++ jquery_dynamic_filter.js
@@ -4,13 +4,24 @@
         
         var settings = $.extend( {
             "searchboxid": null,
-            "columnsToSearchOn": null
+            "columnsToSearchOn": null,
+            "style": "fade",
         }, options);
         
+        var styleChosen = settings["style"].toLowerCase();
         var chosenColumnIndexes = new Array();
-        for (var i=0; i<settings["columnsToSearchOn"].length; i++) {
-             var tmp = $("th").index($("#"+settings["columnsToSearchOn"][i]));
-             chosenColumnIndexes[i] = tmp;
+        
+        if (settings["columnsToSearchOn"] != null) {
+            for (var i=0; i<settings["columnsToSearchOn"].length; i++) {
+                 var tmp = $("th").index($("#"+settings["columnsToSearchOn"][i]));
+                 chosenColumnIndexes[i] = tmp;
+            }
+        } else {
+            //user did not specify columns to search on. make every columns in the table searchable
+            var numberOfColumns = $("th").size();
+            for (var i=0; i<numberOfColumns; i++) {
+                chosenColumnIndexes[i] = i;
+            }
         }
         
         var allRowsInTable = $(this).find("tbody tr");
@@ -26,11 +37,11 @@
                     var currentTDText = $(allTDs[chosenColumnIndexes[k]]).text().toLowerCase();
                     
                     if (currentTDText.indexOf(newFilterString) != -1) {
-                        $(allTDs[chosenColumnIndexes[k]].parentNode).fadeIn();
+                        showRow($(allTDs[chosenColumnIndexes[k]].parentNode));
                         break;
                     } else {
                         if (k == chosenColumnIndexes.length-1) {
-                            $(allTDs[chosenColumnIndexes[k]].parentNode).fadeOut();
+                            hideRow($(allTDs[chosenColumnIndexes[k]].parentNode))
                         }
                     }
                 }
@@ -38,6 +49,22 @@
             }
             
         });
+        
+        function showRow(jqueryNode) {
+            if (styleChosen == "fade") {
+                jqueryNode.fadeIn();
+            } else if (styleChosen == "normal") {
+                jqueryNode.show();
+            }
+        }
+
+        function hideRow(jqueryNode) {
+            if (styleChosen == "fade") {
+                jqueryNode.fadeOut();
+            } else if (styleChosen == "normal") {
+                jqueryNode.hide();
+            }
+        }
 
     };
     
